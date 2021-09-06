aae341371fbd28e55e4de0be0467679001fc2d30
Nayni <naynishah6@gmail.com>
2021-09-06 17:18:58 +0530

    Arithmetic and Assignment Operators

--- app.js
+++ app.js
@@ -1,33 +1,31 @@
-console.log("Hey this is my first piece of Javascript code")
-console.warn("I am a warning")
-console.error("I am an error")
-console.log({name: "Nayni"})
-console.log(true)
-console.log(false)
-console.log(12346)
-console.log("Nayni")
-
-// Data Types
-// Primitive(no methods associated) and Non-primitive(methods and functions associated)
-
-// Primitive Data Types (number, boolean and string)
-var Name = "Nayni";
-console.log(Name);
-// var------> let
-let age = 20.8
+// Arithmetic Operators
+let age = 20
 console.log(age);
-console.log(typeof age);
 
-let brand
-console.log(brand);
+const a = 2
+const b = 7
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a**b);
+console.log(b%a);
+console.log("---------------------------------");
+let c = 0
+c++
+console.log(c);
+c += 5
+console.log(c);
 
-//Non Primitive Data Types (array)
-let arr = [1, 2, 3, 4, 5]
-console.log(arr);
+const d = "hello "
+const e = 55
 
-{
-    Name: "Nayni";
-    Class: "T23";
-    print_name:()=>{console.log("Nayni")}
-}
+console.log(d+e);
 
+// Assignment Operators
+let Name = "Nayni"
+let f = 19
+f+=10
+
+console.log(f);
+console.log(typeof f);
