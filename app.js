36bb6635249421b944f5d665fff37be376e86348
Nayni <naynishah6@gmail.com>
2021-09-06 17:39:13 +0530

    Logical Operators

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
-// Comparison operators
-const a = 30
-const b = 8
-const c = 83
-
-console.log('a > b ',a>b);
-console.log('a < b ',a<b);
-console.log('a == b ',a==b);
-console.log('a == c ',a==c);
-console.log('a >= c ',a>=c);
+// Logical operators
+const a = 20
+const b = 35
+const c = 24
+console.log(c>a && c<b);
 
 console.log('------------------------------');
-const d = '1'
-const e = 1
 
-console.log('d==e', d==e);
-console.log('d===e', d===e);
+console.log('a == b ',a==b);
+console.log('a != b ',a!=b);
+
+console.log('------------------------------');
+
+console.log('Truth Table of AND');
+console.log('false && false', false&&false);
+console.log('true && false', true&&false);
+console.log('false && true', false&&true);
+console.log('true && true', true&&true);
+
+console.log('------------------------------');
+
+console.log('Truth Table of OR');
+console.log('false || false', false||false);
+console.log('true || false', true||false);
+console.log('false || true', false||true);
+console.log('true || true', true||true);
