3c883c0a4278e421714585bcb96089c16d9222c1
Nayni <naynishah6@gmail.com>
2021-09-06 19:53:42 +0530

    Event Listener

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-const element = document.getElementById('select_me')
+const button = document.querySelector('button')
+console.log(button);
+const element = document.querySelector('body')
 console.log(element);
-const element1 = document.getElementsByClassName('heading')
-console.log(element1);
-const element2 = document.getElementsByTagName('h2')
-console.log(element2);
-const element3 = document.querySelector('div#select_me')
-console.log(element3);
-const element4 = document.querySelectorAll('.heading')
-console.log(element4);
+
+
+button.addEventListener('click', () => {
+    element.classList.toggle('dark')
+})
