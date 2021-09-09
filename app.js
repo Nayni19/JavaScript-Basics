3c3a36e3fb661da48991a2c13757acab54d16b19
Nayni <naynishah6@gmail.com>
2021-09-09 19:08:48 +0530

    Array Methods

--- app.js
+++ app.js
@@ -1,58 +1,32 @@
-const rock = document.querySelector('#rock')
-const paper = document.querySelector('#paper')
-const scissor = document.querySelector('#scissor')
-const board = document.querySelector('#board')
-// const handler = (action) => {
-//     console.log("You clicked " + action)
+// var --> global scope
+// let --> local scope
+
+// for (let i = 0; i<=10; i++){
+//     console.log('iteration ' + i);
 // }
-// rock.addEventListener('click', () => {
-//     handler('rock')
-// })
-// paper.addEventListener('click', () => {
-//     handler('paper')})
-// scissor.addEventListener('click', () => {
-//     handler('scissor')})
-// const handler = (action) => {
-//     console.log("You clicked " + action)
+
+// let a = 1
+// while ( a < 10 ){
+//     console.log(a)
+//     a++
 // }
-// rock.addEventListener('click', () => {
-//     handler('rock')
-// })
-// paper.addEventListener('click', () => {
-//     handler('paper')})
-// scissor.addEventListener('click', () => {
-//     handler('scissor')})
 
+const balls = document.getElementsByClassName('ball')
+console.log(balls);
+const body = document.querySelector('body')
 
-const handler = (event) => {
-    const action = event.target.id
-    // if(action === 'rock'){
-    //     board.innerHTML =('Rock crushes Scissor');
-    // }
-    // else if (action === 'paper'){
-    //     board.innerHTML = ('Paper traps Rock');
-    // }
-    // else if (action === 'scissor'){
-    //     board.innerHTML = ('Scrissor cuts Paper');
-    // }
-    // else {
-    //     board.innerHTML = ('Click Something Else');
-    // }
-
-    switch (action){
-        case 'rock':
-            board.innerHTML =('Rock crushes Scissor');
-            break;
-        case 'paper':
-            board.innerHTML = ('Paper traps Rock');
-            break;
-        case 'scissor':
-            board.innerHTML = ('Scrissor cuts Paper');
-            break;
-        default:
-            board.innerHTML = ('Click Something Else');
+const arr = [1,2,3,4,5]
+arr.push(6)
+console.log(arr);
+while (arr.length<10){
+    for(i=6; i<20; i++){
+        arr.push(i)
     }
 }
-rock.addEventListener('click', handler)
-paper.addEventListener('click', handler)
-scissor.addEventListener('click', handler)
+arr.splice(2,5)
+console.log(arr);
+
+arr.forEach((element)=> {
+    console.log("Iteration");
+    console.log(element);
+})
