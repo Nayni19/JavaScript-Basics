a3d27370b462fad470d2e26592a4515794d5dd95
Nayni <naynishah6@gmail.com>
2021-09-09 18:24:29 +0530

    Event Listener

--- app.js
+++ app.js
@@ -1,19 +1,58 @@
-// const rock= document.querySelector('#rock')
-// console.log(rock);
+const rock = document.querySelector('#rock')
+const paper = document.querySelector('#paper')
+const scissor = document.querySelector('#scissor')
+const board = document.querySelector('#board')
+// const handler = (action) => {
+//     console.log("You clicked " + action)
+// }
+// rock.addEventListener('click', () => {
+//     handler('rock')
+// })
+// paper.addEventListener('click', () => {
+//     handler('paper')})
+// scissor.addEventListener('click', () => {
+//     handler('scissor')})
+// const handler = (action) => {
+//     console.log("You clicked " + action)
+// }
+// rock.addEventListener('click', () => {
+//     handler('rock')
+// })
+// paper.addEventListener('click', () => {
+//     handler('paper')})
+// scissor.addEventListener('click', () => {
+//     handler('scissor')})
 
-// const paper= document.querySelector('#paper')
-// console.log(paper);
 
-// const scissor= document.querySelector('#scissor')
-// console.log(scissor);
+const handler = (event) => {
+    const action = event.target.id
+    // if(action === 'rock'){
+    //     board.innerHTML =('Rock crushes Scissor');
+    // }
+    // else if (action === 'paper'){
+    //     board.innerHTML = ('Paper traps Rock');
+    // }
+    // else if (action === 'scissor'){
+    //     board.innerHTML = ('Scrissor cuts Paper');
+    // }
+    // else {
+    //     board.innerHTML = ('Click Something Else');
+    // }
 
-// Function
-function logName(name = "Enter your Name"){
-    console.log(name);
+    switch (action){
+        case 'rock':
+            board.innerHTML =('Rock crushes Scissor');
+            break;
+        case 'paper':
+            board.innerHTML = ('Paper traps Rock');
+            break;
+        case 'scissor':
+            board.innerHTML = ('Scrissor cuts Paper');
+            break;
+        default:
+            board.innerHTML = ('Click Something Else');
+    }
 }
-logName('Nayni Shah');
-
-const add = (a=0, b=0) => {
-    return a+b;
-}
-console.log(add(12,23));
+rock.addEventListener('click', handler)
+paper.addEventListener('click', handler)
+scissor.addEventListener('click', handler)
