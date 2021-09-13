a60641033836479acfb4f51340d34391424a72c1
Nayni <naynishah6@gmail.com>
2021-09-13 16:33:54 +0530

    Promise

--- app.js
+++ app.js
@@ -1,10 +1,38 @@
 console.log("Merged Class 2 ES6");
 
-let ranks = [1, 2, 3, 4, 5 ,6];
+let concert = true;
 
-let newfilterarr = ranks.filter((e, index) => {
-    console.log(e)
-    if(e%2===0)
-        return e
-})
-console.log(newfilterarr);
+let attendConcert = new Promise(function (resolve, reject) {
+    setTimeout(() => {
+        if (concert) {
+            resolve("BOB ATTENDED THE CONCERT");
+        }
+        else {
+            reject("BOB FAILED TO ATTEND THE CONCERT");
+        }
+    }, 2000);
+});
+console.log(attendConcert)
+
+// attendConcert.then((data)=>console.log(data));
+// attendConcert.catch((error)=>console.log(error));
+
+attendConcert.then((data)=>console.log(data)).catch((error)=>console.log(error));
+
+console.log("HI, I WON'T WAIT FOR ANYONE")
+console.log("HI, I AM DYNING, I DON'T WANT TO WAIT")
+
+
+// Arrow function
+// let attendConcert = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         if (concert) {
+//             resolve("BOB ATTENDED THE CONCERT");
+//         }
+//         else {
+//             reject("BOB FAILED TO ATTEND THE CONCERT");
+//         }
+//     }, 2000);
+// });
+
+// console.log(attendConcert)
