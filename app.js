055308ff8dd9cfe83104fc726b14251758cde6cd
Nayni <naynishah6@gmail.com>
2021-09-13 17:01:56 +0530

    Async-Await

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 console.log("Merged Class 2 ES6");
 
-let concert = true;
+let concert = false;
 
 let attendConcert = new Promise(function (resolve, reject) {
     setTimeout(() => {
@@ -14,25 +14,17 @@ let attendConcert = new Promise(function (resolve, reject) {
 });
 console.log(attendConcert)
 
-// attendConcert.then((data)=>console.log(data));
-// attendConcert.catch((error)=>console.log(error));
+// attendConcert.then((data)=>console.log(data)).catch((error)=>console.log(error));
 
-attendConcert.then((data)=>console.log(data)).catch((error)=>console.log(error));
-
-console.log("HI, I WON'T WAIT FOR ANYONE")
-console.log("HI, I AM DYNING, I DON'T WANT TO WAIT")
-
-
-// Arrow function
-// let attendConcert = new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//         if (concert) {
-//             resolve("BOB ATTENDED THE CONCERT");
-//         }
-//         else {
-//             reject("BOB FAILED TO ATTEND THE CONCERT");
-//         }
-//     }, 2000);
-// });
-
-// console.log(attendConcert)
+async function afunc(){
+  try{
+    let result = await attendConcert;
+    console.log(result);
+    return result;
+  }
+  catch(e){
+    console.log(e);
+  }
+  
+}
+console.log("afunc",afunc());
