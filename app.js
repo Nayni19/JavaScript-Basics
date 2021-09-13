533c391407e375e11a3c23fd7c4c4ef93eab4fd0
Nayni <naynishah6@gmail.com>
2021-09-13 15:43:54 +0530

    filter

--- app.js
+++ app.js
@@ -2,22 +2,9 @@ console.log("Merged Class 2 ES6");
 
 let ranks = [1, 2, 3, 4, 5 ,6];
 
-console.log(
-  ranks.map((e, index) => {
-    return e * 100;
-  })
-);
-
-console.log(
-  ranks.forEach((e, index) => {
-    console.log(e*100);
-    return e * 100;
-  })
-);
-
-let newmaparr = ranks.map((e, index) => {
+let newfilterarr = ranks.filter((e, index) => {
     console.log(e)
     if(e%2===0)
         return e
 })
-console.log(newmaparr);
+console.log(newfilterarr);
